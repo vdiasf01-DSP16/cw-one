acbabf883de69e65a9dcb3d8f988bf64fff00f63
Vasco Horta <diasfonseca@live.com>
2016-01-31 11:58:57 +0000

    LinInstruction comment updates
    Updating LinInstruction comment updates

--- SML/src/sml/LinInstruction.java
+++ SML/src/sml/LinInstruction.java
@@ -1,19 +1,43 @@
 package sml;
 
 /**
- * This class ....
- *
+ * This class executes the instruction of setting a register
+ * with the given value.
+ * 
+ * e.g.: 'Label' 'lin' 'R1' '#123'
+ *       R1 = 123
+ * 
  * @author someone
  */
-
 public class LinInstruction extends Instruction {
+	
+	/**
+	 * The register to update.
+	 */
     private int register;
+    
+    /**
+     * The value set to register.
+     */
     private int value;
 
+    /**
+     * The super constructor for label and operation instruction code.
+     * 
+     * @param label
+     * @param opcode
+     */
     public LinInstruction(String label, String opcode) {
         super(label, opcode);
     }
 
+    /**
+     * Constructor for the expected complete line of code.
+     * 
+     * @param label
+     * @param register
+     * @param value
+     */
     public LinInstruction(String label, int register, int value) {
         super(label, "lin");
         this.register = register;
@@ -21,11 +45,19 @@ public class LinInstruction extends Instruction {
 
     }
 
+    /**
+     * Execute the lin instruction on the Machine.
+     */
     @Override
     public void execute(Machine m) {
+    	// The core of the lin code, setting given register
+    	// with the given value.
         m.getRegisters().setRegister(register, value);
     }
 
+    /**
+     * Printing out a user-friendly version of what was done in this instruction.
+     */
     @Override
     public String toString() {
         return super.toString() + " R" + register + " = " + value;
